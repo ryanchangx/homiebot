64049fd4b5c3976425929ccd21ff92e5f87367ed
Ryan Chang <ryanchangx@gmail.com>
2020-11-07 08:51:14 -0800

    Added shibe command

--- bot.py
+++ bot.py
@@ -17,4 +17,14 @@ bot = commands.Bot(command_prefix='&')
 async def on_ready():
     print(f'{bot.user.name} joined the homies!')
 
+@bot.command(name='shibe',help='get a good boy')
+async def dog(ctx):
+    async with ctx.channel.typing():
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true') as r:
+                data = await r.json()
+                embed=discord.Embed(title="awoo")
+                embed.set_image(url=data[0])
+                await ctx.send(embed=embed)
+
 bot.run(TOKEN)
